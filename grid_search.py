bde882fa0103b94e373eb1e46c802f07c80e0f1b
Keto Zhang <keto.zhang@gmail.com>
2020-03-02 18:04:27 -0800

    Add v2 and v3 models, update jupyter notebook

--- grid_search.py
+++ grid_search.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import numpy as np
 import pickle as pkl
 import matplotlib.pyplot as plt
@@ -8,27 +9,29 @@ from sklearn.model_selection import ParameterGrid
 from dataloader import import_kaepora
 
 PROJECT_PATH = Path(__file__).resolve().parent
-RESULTS_PATH = PROJECT_PATH / 'results'
-RNG = np.random.RandomState(seed=822)
+RESULTS_PATH = PROJECT_PATH / "results"
 
 # [mu_lv, sigma_lv, mu_hv, sigma_hv, mixing_parameter]
-BIMODAL_PARAMS = np.loadtxt(str(RESULTS_PATH/"bimodal_params.csv"), delimiter=",")
+BIMODAL_PARAMS = np.loadtxt(str(RESULTS_PATH / "bimodal_params.csv"), delimiter=",")
 
 
-def model(lv_dist, theta, delta_v, **kwargs):
-    sample_size = kwargs.pop('sample_size', 10000)
+def model_v2(lv_dist, theta, delta_v, **kwargs):
+    """Two population model --- one spherical and one asymmetric."""
+    rng = np.random.RandomState(seed=822)
+    sample_size = kwargs.pop("sample_size", 10000)
     g = BIMODAL_PARAMS[4]  # mixing parameter
     lv_size = int(sample_size * g)
     hv_size = sample_size - lv_size
 
     # Take sample from the low velocity distribution
-    lv = lv_dist.rvs(hv_size, random_state=RNG)
+    lv = lv_dist.rvs(hv_size, random_state=rng)
 
-    # Take sample of the line of sight
-    los = RNG.uniform(0, 180, len(lv))
+    # Take sample of the line of sight using uniform RV to spherical transformation
+    u = np.random.uniform(0, 1, hv_size)
+    los = np.degrees(np.arccos(2 * u - 1))
 
     # hv = lv + delta_v  # constant velocity
-    hv = lv + delta_v*((theta - los) / theta)  # linear velocity
+    hv = lv + delta_v * ((theta - los) / theta)  # linear velocity
 
     # For each data point in hv and lv:
     # Use hv's data point if line of sight is within theta otherwise use lv data point
@@ -36,13 +39,35 @@ def model(lv_dist, theta, delta_v, **kwargs):
     hv = np.choose(lv_cond, [hv, lv])
 
     # Now take a sample from the low velocity distribution and do nothing to it
-    lv = lv_dist.rvs(lv_size, random_state=RNG)
+    lv = lv_dist.rvs(lv_size, random_state=rng)
 
     v_sim = np.hstack((lv, hv))
     return v_sim
 
 
-def simulate(v_data, lv_dist, param_grid, **kwargs):
+def model_v3(lv_dist, theta, delta_v, **kwargs):
+    """One population model --- all asymmetric."""
+    rng = np.random.RandomState(seed=822)
+    sample_size = kwargs.pop("sample_size", 10000)
+
+    # Take sample from the low velocity distribution
+    lv = lv_dist.rvs(sample_size, random_state=rng)
+
+    # Take sample of the line of sight using uniform RV to spherical transformation
+    u = np.random.uniform(0, 1, sample_size)
+    los = np.degrees(np.arccos(2 * u - 1))
+
+    # hv = lv + delta_v  # constant velocity
+    hv = lv + delta_v * ((theta - los) / theta)  # linear velocity
+
+    # For each data point in hv and lv:
+    # Use hv's data point if line of sight is within theta otherwise use lv data point
+    lv_cond = los > theta
+    v_sim = np.choose(lv_cond, [hv, lv])
+    return v_sim
+
+
+def simulate(model, v_data, lv_dist, param_grid, **kwargs):
     """Simualte and return score.
 
     Parameters
@@ -63,12 +88,16 @@ def simulate(v_data, lv_dist, param_grid, **kwargs):
         params -- model parameters (delta_v, theta)
         v_sim -- resultant simulated velocity data
     """
-    # Expand the parameter grids to a list of dict elements.
-    # Each element is a single set model parameters
+    # Expand the parameter grids to a list of dict elements
+    # with each element being a single set model parameters
+    # [
+    #    {'theta': ..., 'delta_v': ...},
+    #    {'theta': ..., 'delta_v': ...},
+    #    ...
+    # ]
     param_grid = ParameterGrid(param_grid)
 
     # The following scores will be stored for each set of parameters
-    all_scores = {"ks": [], "pvalue": [], "params": []}
     scores = {"ks": [], "pvalue": [], "params": [], "v_sim": []}
 
     nparams = len(param_grid)
@@ -77,9 +106,9 @@ def simulate(v_data, lv_dist, param_grid, **kwargs):
 
     for i, params in enumerate(param_grid):
         # Observe the model and return the observed (simulated) velocity data
-        v_sim = model(lv_dist, params['theta'], params['delta_v'], **kwargs)
+        v_sim = model(lv_dist, params["theta"], params["delta_v"], **kwargs)
 
-        # Compute KS values
+        # Compute KS and p-value
         ks, pvalue = ks_2samp(v_data, v_sim)
 
         # if pvalue >= 0.05:
@@ -90,9 +119,6 @@ def simulate(v_data, lv_dist, param_grid, **kwargs):
         #     plt.show()
 
         # Keeping only the top 100 models chosen by their KS score
-        all_scores["ks"].append(ks)
-        all_scores["pvalue"].append(pvalue)
-        all_scores["params"].append(params)
         if i < 100:
             scores["ks"].append(ks)
             scores["pvalue"].append(pvalue)
@@ -111,7 +137,7 @@ def simulate(v_data, lv_dist, param_grid, **kwargs):
         end = time.perf_counter()
         elapsed_times.append(end - start)
         mean_step_period = np.mean(elapsed_times)
-        if (i % (nparams//100) == 0 or i > nparams-10):
+        if i % (nparams // 100) == 0 or i > nparams - 10:
             print(f"{params} -> {ks, pvalue}")
             print(f"Step [{i}/{nparams}] ", end="")
             print(f"ETA: {(nparams - i) * mean_step_period:.2f} secs")
@@ -119,28 +145,31 @@ def simulate(v_data, lv_dist, param_grid, **kwargs):
 
     # Conver score from Python list to numpy array
     scores = {k: np.array(v) for k, v in scores.items()}
-    all_scores = {k: np.array(v) for k, v in scores.items()}
-    return scores, all_scores
+    return scores
 
 
 if __name__ == "__main__":
     # Velocity data
     v_data = import_kaepora()["v_siII"]
 
-    # Create Gaussian distribution for low velocity
+    # Create Gaussian distribution describing low velocity group I
     lv_params = BIMODAL_PARAMS[:2]
     lv_dist = norm(*lv_params)
 
-    # Set the range for each parameter in the parameter grid
+    # Set the range for each parameter in the parameter space
     param_grid = {
         "theta": np.arange(0, 181, 5),
         "delta_v": np.arange(3000, 7501, 500),
     }
 
-    # Start simulation and save scores
-    scores, all_scores = simulate(v_data, lv_dist, param_grid, sample_size=int(1e5))
-    with open(RESULTS_PATH/"scores.pkl", "wb") as f:
-        # pkl.dump({k: v[:10] for k, v in scores.items()}, f)
+    # Simulation v2
+    # start simulation and save scores
+    scores = simulate(model_v2, v_data, lv_dist, param_grid, sample_size=int(1e5))
+    with open(RESULTS_PATH / "scores_v2.pkl", "wb") as f:
         pkl.dump(scores, f)
-    with open(RESULTS_PATH/"all_scores.pkl", "wb") as f:
-        pkl.dump(all_scores, f)
+
+    # Simulation v3
+    # start simulation and save scores
+    scores = simulate(model_v3, v_data, lv_dist, param_grid, sample_size=int(1e5))
+    with open(RESULTS_PATH / "scores_v3.pkl", "wb") as f:
+        pkl.dump(scores, f)
